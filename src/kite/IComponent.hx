50ea72832c913f6e4000e8727b7ddd17371dbf44
Xhi <xhi2art@gmail.com>
2016-11-27 21:40:47 +0200

    changed comment type

--- src/kite/IComponent.hx
+++ src/kite/IComponent.hx
@@ -1,6 +1,8 @@
 package kite;
 
-// Components automatically define a private __uid__ method that returns 
-// an Int with a unique value for each component type.
+/**
+    Components automatically define a private __uid__ method that returns 
+    an Int with a unique value for each component type.
+**/
 @:autoBuild(kite.macro.MakeComponent.generate())
 interface IComponent {}
